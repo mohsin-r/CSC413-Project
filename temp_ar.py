2829d54ed71294a9be02f3147812ebd0638c90ff
Zexi Lv <61070237+Cowsssssss@users.noreply.github.com>
2024-04-18 03:39:25 -0400

    updated autoregression models

--- temp_ar.py
+++ temp_ar.py
@@ -2,24 +2,41 @@ import pandas as pd
 from sklearn.impute import SimpleImputer
 from statsmodels.tsa.ar_model import AutoReg
 from sklearn.metrics import mean_squared_error
+from matplotlib import pyplot as plt
 
-# process the data
-temp_data = pd.read_csv("data/temperature.csv")['Toronto'].to_frame()
-imputer = SimpleImputer(strategy='mean')
-imputed_data = imputer.fit_transform(temp_data)
+if __name__ == '__main__':
+    # process the data
+    temperature_data = pd.read_csv("data/temperature.csv").drop(columns=["datetime"])
+    imputer = SimpleImputer(strategy='mean')
+    imputed_data = pd.DataFrame(imputer.fit_transform(temperature_data))
+    imputed_data.columns = temperature_data.columns
+    imputed_data.index = temperature_data.index
 
-# training split
-split = int(len(temp_data) * 80 / 100)
-train_data = imputed_data[:split]
-test_data = imputed_data[split:]
+    # training split, 70:15:15
+    split1 = int(len(temperature_data) * 70 / 100)
+    split2 = int(len(temperature_data) * 85 / 100)
+    train_data = imputed_data.iloc[0:split1]
+    val_data = imputed_data.iloc[split1:split2]
+    test_data = imputed_data.iloc[split2:]
 
-# create model
-lags = 3 # error doesn't change too much, but slowly decreases with more lags
-model = AutoReg(train_data, lags)
-model_fit = model.fit()
+    # test different lags on validation set
+    errors = []
+    for lags in range(80):
+        err_sum = 0
 
-predictions = model_fit.predict(start=lags, end=len(train_data)-1)#start=len(train_data), end=(len(train_data) + len(test_data) - 1))
+        for city in imputed_data.columns:
+            # create model
+            model = AutoReg(train_data[city], lags)
+            model = model.fit()
 
-# compute error
-mse = mean_squared_error(train_data[lags:], predictions)
-print(f'Lags: {lags}, MSE: {mse}')
+            # compute error on validation set
+            predictions = model.predict(start=len(train_data), end=len(train_data) + len(val_data)-1)
+            err_sum += mean_squared_error(val_data[city], predictions)
+
+        error = err_sum / len(imputed_data.columns)
+        errors.append(error)
+    plt.plot(errors, label="Mean Squared Error")
+    plt.xlabel("Lags")
+    plt.ylabel("Mean Squared Error")
+    plt.title("Lags vs Mean Squared Error for Temperature AR")
+    plt.savefig("ar_plots/temp_ar.jpg")
